0c7f520268e2201c7de7217d68e45aaeef57feff
jfcaraujo <jfcaraujo99@gmail.com>
2019-05-29 19:31:58 +0100

    Finished bird

--- parte 2/MyBird.js
+++ parte 2/MyBird.js
@@ -11,36 +11,102 @@ class MyBird extends CGFobject {
     }
     init() {
         //create materials
-        this.marbleMaterial = new CGFappearance(this.scene);
-        this.marbleMaterial.setAmbient(0.5, 0.5, 0.5, 1);
-        this.marbleMaterial.setDiffuse(0.5, 0.5, 0.5, 1);
-        this.marbleMaterial.setSpecular(1, 1, 1, 1);
-        this.marbleMaterial.setShininess(10.0);
-        this.marbleMaterial.loadTexture('textures/Marble.jpg');
-        this.marbleMaterial.setTextureWrap('REPEAT', 'REPEAT');
+        this.black = new CGFappearance(this.scene);
+        this.black.setAmbient(0, 0, 0, 1);
+        this.black.setDiffuse(0, 0, 0, 1);
+        this.black.setSpecular(0, 0, 0, 1);
+        this.black.setShininess(10.0);
 
-        this.blueTiledRoofMaterial = new CGFappearance(this.scene);
-        this.blueTiledRoofMaterial.setAmbient(0.5, 0.5, 0.5, 1);
-        this.blueTiledRoofMaterial.setDiffuse(0.9, 0.9, 0.9, 1);
-        this.blueTiledRoofMaterial.setSpecular(0, 0, 0, 1);
-        this.blueTiledRoofMaterial.setShininess(10.0);
-        this.blueTiledRoofMaterial.loadTexture('textures/BlueTileRoof.jpg');
-        this.blueTiledRoofMaterial.setTextureWrap('REPEAT', 'REPEAT');
+        this.yellow = new CGFappearance(this.scene);
+        this.yellow.setAmbient(1, 1, 0, 1);
+        this.yellow.setDiffuse(1, 1, 0, 1);
+        this.yellow.setSpecular(1, 1, 0, 1);
+        this.yellow.setShininess(10.0);
 
         //create objects
-        this.cube = new MyUnitCube(this.scene);
-        this.prism = new MyPrism(this.scene, 7, 3, 0.5);
+        this.sphere = new MySphere(this.scene, 0.7, 6, 5);
+        this.quad = new MyQuad(this.scene);
+        this.triangle = new MyTriangle(this.scene);
         this.pyramid = new MyPyramid(this.scene, 4);
-        this.cylinder = new MyCylinder(this.scene, 5, 0.1, 0.4);
+        this.cylinder = new MyCylinder(this.scene, 5, 0.1, 0.15);
     }
 
     display() {
+        /*this.scene.pushMatrix();
+        this.scene.translate(0, 0, -0.5);
+        this.scene.scale(2, 2, 2);
+        this.scene.rotate(Math.PI / 2, 1, 0, 0);
+        this.quad.display();//test size
+        this.scene.popMatrix();*/
 
         this.scene.pushMatrix();
-        this.cube.display();
-        this.scene.translate(0,1,0);
-        this.cylinder.display();
+
+        this.scene.scale(0.6, 0.65, 0.6);
+        this.scene.pushMatrix();
+        this.sphere.display();//head
+        this.scene.scale(1, 1, 1.3);
+        this.scene.translate(0, -0.5, -1);
+        this.sphere.display();//body
         this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.scene.translate(0, -0.5, -2.1);
+        this.scene.rotate(Math.PI * 3 / 4, 0, 1, 0);
+        this.triangle.display();//tail
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.scene.translate(-0.8, -0.4, -1.25);
+        this.scene.rotate(-Math.PI / 4, 0, 0, 1);
+        this.scene.rotate(-Math.PI / 2, 1, 0, 0);
+        this.scene.scale(0.5, 0.5, 1);
+        this.quad.display();//right wing square
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.scene.translate(-0.97, -0.22, -1.5);
+        this.scene.rotate(Math.PI / 4, 0, 0, 1);
+        this.scene.rotate(-Math.PI / 2, 0, 1, 0);
+        this.triangle.display();//right wing triangle
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.scene.translate(0.8, -0.4, -1.25);
+        this.scene.rotate(Math.PI / 4, 0, 0, 1);
+        this.scene.rotate(-Math.PI / 2, 1, 0, 0);
+        this.scene.scale(0.5, 0.5, 1);
+        this.quad.display();//left wing square
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.scene.translate(0.97, -0.22, -1.5);
+        this.scene.rotate(Math.PI * 3 / 4, 0, 0, 1);
+        this.scene.rotate(-Math.PI / 2, 0, 1, 0);
+        this.triangle.display();//left wing triangle
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.black.apply();
+        this.scene.translate(-0.5, 0.25, 0.15);
+        this.scene.rotate(Math.PI / 2, 0, 0, 1);
+        this.cylinder.display();//right eye
+        this.scene.popMatrix();
+        this.scene.pushMatrix();
+        this.black.apply();
+        this.scene.translate(0.5, 0.25, 0.15);
+        this.scene.rotate(-Math.PI / 2, 0, 0, 1);
+        this.cylinder.display();//left eye
+        this.scene.popMatrix();
+        this.scene.pushMatrix();
+        this.yellow.apply();
+        this.scene.translate(0, 0, 0.6);
+        this.scene.rotate(Math.PI / 2, 1, 0, 0);
+        this.scene.scale(0.25, 0.25, 0.25);
+        this.pyramid.display();//beak
+        this.scene.popMatrix();
+
+        this.scene.popMatrix();
+
     }
 }
 
--- parte 2/MyPyramid.js
+++ parte 2/MyPyramid.js
@@ -66,14 +66,7 @@ class MyPyramid extends CGFobject {
         this.primitiveType = this.scene.gl.TRIANGLES;
         this.initGLBuffers();
     }
-    
-    updateBuffers(complexity){
-        this.slices = 3 + Math.round(9 * complexity); //complexity varies 0-1, so slices varies 3-12
-
-        // reinitialize buffers
-        this.initBuffers();
-        this.initNormalVizBuffers();
-    }
+  
 }
 
 
